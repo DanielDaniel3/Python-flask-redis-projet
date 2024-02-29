79d34f0c7d1bfc2f953dbdb92d63c024c2b2db4c
Administrator <daniel.ntaye@sahanalytics.com>
2024-02-29 18:57:45 +0000

    Tests de charge

--- .github/workflows/my-load-test.js
+++ .github/workflows/my-load-test.js
@@ -1,10 +1,20 @@
-
-import http from "k6/http";
+import http from 'k6/http';
+import { sleep } from 'k6';
 
 export const options = {
-  iterations: 1,
+  vus: 10,
+  duration: '30s',
+  ext: {
+    loadimpact: {
+      // Project: Tests k6
+      projectID: 3660733,
+      // Test runs with the same name groups test runs together.
+      name: 'Test (29/02/2024-18:46:01)'
+    }
+  }
 };
 
-export default function () {
-  const response = http.get("https://test-api.k6.io/public/crocodiles/");
+export default function() {
+  http.get('http://test.k6.io');
+  sleep(1);
 }
